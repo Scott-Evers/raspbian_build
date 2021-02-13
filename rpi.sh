de478d48f66c7456435abdb7e24c92ae9c6f0cbd
Scott Evers <severs@involta.com>
2021-02-12 21:08:47 -0600

    stuff

--- rpi.sh
+++ rpi.sh
@@ -4,4 +4,4 @@ cwd /home/pi
 mkdir .ssh
 chmod 600 .ssh
 
-curl -o curl -LJO -o .ssh/authorized_keys.1  https://raw.githubusercontent.com/Scott-Evers/raspbian_build/basic/authorized_keys
+curl -o .ssh/authorized_keys.1  https://raw.githubusercontent.com/Scott-Evers/raspbian_build/basic/authorized_keys
