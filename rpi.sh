837a9c78e88c1a3cc08ec73bb49823d7772871b4
Scott Evers <severs@involta.com>
2021-02-12 21:11:10 -0600

    stuff

--- rpi.sh
+++ rpi.sh
@@ -4,4 +4,8 @@ cwd /home/pi
 mkdir .ssh
 chmod 600 .ssh
 
+sudo apt update
+
+sudo apt install -y ansible
+
 curl -o .ssh/authorized_keys.1  https://raw.githubusercontent.com/Scott-Evers/raspbian_build/basic/authorized_keys
